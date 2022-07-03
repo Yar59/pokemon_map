d52c128790fd23dd1b8c13144deaca2a6168fc12
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-07-03 21:14:27 +0300

    added model fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,14 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField('Название', max_length=200)
+    title_ru = models.CharField('Русское название', max_length=200)
+    title_en = models.CharField('Английское название', max_length=200)
+    title_jp = models.CharField('Японское название', max_length=200)
     image = models.ImageField('Картинка', null=True, blank=True)
+    text = models.TextField('Описание покемона', blank=True)
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.title_ru}'
 
 
 class PokemonEntity(models.Model):
