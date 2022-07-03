576b478bfd4a190c5b17f426678ee314e732243c
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-07-03 19:58:42 +0300

    show pokemon page from db

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -65,23 +66,26 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
+    try:
+        pokemon = Pokemon.objects.get(id=pokemon_id)
+    except ObjectDoesNotExist:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
+    pokemon_entities = PokemonEntity.objects.filter(pokemon=pokemon)
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
-        add_pokemon(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
-        )
+    for pokemon_entity in pokemon_entities:
+        appeared_at = timezone.localtime(pokemon_entity.appeared_at)
+        disappeared_at = timezone.localtime(pokemon_entity.disappeared_at)
+        time_now = timezone.now()
+        if appeared_at < time_now < disappeared_at:
+            image_url = request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+            add_pokemon(
+                folium_map,
+                pokemon_entity.latitude,
+                pokemon_entity.longitude,
+                image_url
+            )
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(), 'pokemon': pokemon
