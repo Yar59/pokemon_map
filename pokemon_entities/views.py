c6a00b69f26ed9358ae0c394554d4a3b74c3d780
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-07-06 00:33:22 +0300

    deleted unused code

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -27,8 +27,6 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     pokemon_entities = PokemonEntity.objects.all()
